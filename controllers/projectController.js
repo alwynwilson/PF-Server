29f5fceb6b8ca8ea960fd2f12cc2a8e072ea908c
Alwyn <alwynwilson187@gmail.com>
2024-06-18 14:00:11 +0530

    server index update

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -24,3 +24,37 @@ exports.addProjectController = async (req,res)=>{
         res.status(401).json(err)
     }
 }
+
+// home project
+exports.getHomeProjects = async (req,res)=>{
+    console.log("Inside getHomeProjects");
+    try{
+        const homeProjects = await projects.find().limit(3)
+        res.status(200).json(homeProjects)
+    }catch(err){
+        res.status(401).json(err)
+    }
+}
+
+//All user
+exports.allProjectsController = async (req,res)=>{
+    console.log("Inside allProjects");
+    try{
+        const allProjects = await projects.find()
+        res.status(200).json(allProjects)
+    }catch(err){
+        res.status(401).json(err)
+    }
+}
+
+//get user Project
+exports.getuserProjectController = async (req,res)=>{
+    console.log("Inside getuserProjectProjects");
+    const userId = req.payload
+    try{
+        const userProjects = await projects.find({userId})
+        res.status(200).json(userProjects)
+    }catch(err){
+        res.status(401).json(err)
+    }
+}
--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,9 @@ const userController = require('../controllers/userController')
 const projectController = require('../controllers/projectController')
 const jwtMiddleware = require('../middlewares/jwtMiddleware')
 const multerMiddleware = require('../middlewares/multerMiddleware')
+const getHomeProjects = require('../controllers/projectController')
+const allProjectsController = require('../controllers/projectController')
+const getuserProjectController = require('../controllers/projectController')
 
 const router = new express.Router()
 
@@ -16,5 +19,13 @@ router.post('/login',userController.loginController)
 // add project
 router.post('/project/add',jwtMiddleware,multerMiddleware.single('projectImg') ,projectController.addProjectController)
 
+//home project
+router.get('/get-home-projects',projectController.getHomeProjects)
 
-module.exports = router
+//All projects
+router.get('/all-Projects',jwtMiddleware,projectController.allProjectsController)
+
+//user project
+router.get('/user-projects',jwtMiddleware,projectController.getuserProjectController)
+
+module.exports = router 
